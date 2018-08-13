b1d85d9b282bd73b46f0fdf9b135afa945a75366
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-13 17:35:49 +0900

    Finish 3章

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  root 'static_pages#home'
 end
