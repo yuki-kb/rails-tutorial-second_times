9a2dcee0e8ab6fc3005d03722ce1ec50e397abc0
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-16 15:48:24 +0900

    Create user_test that is precence of user

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_024633) do
+ActiveRecord::Schema.define(version: 2018_08_16_063509) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
 end
