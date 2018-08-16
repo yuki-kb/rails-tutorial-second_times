fa530f8fe0cb94bbeb5e3e437dbebe0410fedb19
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-16 20:38:24 +0900

    Create the test that is presence & length of password

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -11,4 +11,5 @@ class User < ApplicationRecord
 					format: {with:VALID_USER_ID_REGEX},
 					uniqueness: {case_sensitive: false}
 	has_secure_password
+	validates :password,presence: true,length: {minimum: 6}
 end
--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -111,4 +111,14 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_user_id.downcase,@user.reload.user_id
   end
+
+  test "password should be present and not be blank" do
+    @user.password = @user.password_confirmation = "  "
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "0"
+    assert_not @user.valid?
+  end
 end
