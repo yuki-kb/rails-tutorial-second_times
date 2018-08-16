87c88f9b337ba91ec21ca1a46242767ca4a86ee2
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-16 16:08:03 +0900

    Create user_test that is length of user

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-	validates :name,presence: true
+	validates :name,presence: true,
 					length: {maximum: 50}
-	validates :email,presence: true
+	validates :email,presence: true,
 					length: {maximum: 255}
-	validates :user_id,presence: true
+	validates :user_id,presence: true,
 					length: {maximum: 50}
 end
