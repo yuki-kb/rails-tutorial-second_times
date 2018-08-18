2415ab1440a3c50e96519e81b6deb4b0537844d5
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-18 11:03:10 +0900

    Create user_params for security in params

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		#保存成功
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:name,:email,:user_id,
+  									:password,:password_confirmation)
+  	end
+  	
 end
