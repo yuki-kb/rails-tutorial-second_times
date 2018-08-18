5f4c7c13b6e165372d036686e62ecb0da64ee77a
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-18 21:48:55 +0900

    Create integration test in creating a new user

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -7,16 +7,34 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	assert_select 'form[action="/signup"]'
   	assert_no_difference 'User.count' do
   		post signup_path,params: {user: {name: "",
-  										email: "user@invalid",
-  										password: "foo",
-  										password_confirmation: "bar"}}
+  										                email: "user@invalid",
+                                      user_id: "invalid.user",
+  										                password: "foo",
+  										                password_confirmation: "bar"}}
   	end
   	assert_template 'users/new'
-  	# した二つはエラーようPartialまんまのテスト
+  	# した二つはエラーよるPartialまんまのテスト
   	assert_select "div#error_explanation div.alert"
   	assert_select "div#error_explanation ul"
   	# 無効な内容が送信されて元のページに戻されるときにRailsが返してくれる
   	# エラー用divタグのCSSクラスのfield_with_errorsに関するテスト
   	assert_select "div .field_with_errors"
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_difference 'User.count',1 do
+      post signup_path,params: {user: {name: "Eample User",
+                                      email: "user@example.com",
+                                      user_id: "valid.user",
+                                      password: "password",
+                                      password_confirmation: "password"}}
+    end
+    follow_redirect!
+    #指定されたリダイレクトに移動↑
+    assert_template 'users/show'
+    #flashが表示されるか確認
+    assert flash.any?
   end
 end
