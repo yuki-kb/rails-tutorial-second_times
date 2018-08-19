0122eaf085481501706d2fa89585a8a832133766
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-19 12:33:48 +0900

    Create log_in method in sessions_helper

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email:params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  		#ユーザーページにリダイレクト
+  		log_in user
+      flash[:succsess] = 'you could Log in Correctly'
+      redirect_to user
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
