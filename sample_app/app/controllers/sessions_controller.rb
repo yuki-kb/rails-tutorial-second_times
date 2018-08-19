1a6a7cf544aa9b9895fdeb8a63ad68c2d135580a
yuki kakubari <yuki.0730.kb@gmail.com>
2018-08-19 11:55:35 +0900

    Create actions of session

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email:params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		#ユーザーページにリダイレクト
+  	else
+  		#エラーメッセ表示
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  end
+
 end
